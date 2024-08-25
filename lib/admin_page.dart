712b91abd60b35425ef9daf248317981ea2a9020
Arthaloith <nguyenmanhhung1904@gmail.com>
2024-08-25 20:03:09 +0700

    sus2

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -2,6 +2,9 @@ import 'package:amogus/feedback_detail.dart';
 import 'package:amogus/ui/app_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/theme_provider.dart';
 
 class AdminPage extends StatefulWidget {
   @override
@@ -10,34 +13,42 @@ class AdminPage extends StatefulWidget {
 
 class _AdminPageState extends State<AdminPage> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customAppBar(context, 'Feedback List'),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('feedback').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              DocumentSnapshot document = snapshot.data!.docs[index];
-              return ListTile(
-                title: Text(document['email']),
-                subtitle: Text(document['feedback']),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            FeedbackDetailPage(document: document)),
+  Widget build(
+    BuildContext context,
+  ) {
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      return Scaffold(
+        appBar: customAppBar(context, 'Feedback List'),
+        body: Theme(
+          data: themeProvider.theme,
+          child: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('feedback').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot document = snapshot.data!.docs[index];
+                  return ListTile(
+                    title: Text(document['email']),
+                    subtitle: Text(document['feedback']),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                FeedbackDetailPage(document: document)),
+                      );
+                    },
                   );
                 },
               );
             },
-          );
-        },
-      ),
-    );
+          ),
+        ),
+      );
+    });
   }
 }
